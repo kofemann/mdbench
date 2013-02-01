8631e1e560902efd6b1d8151dd3fff4964cef421
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2013-02-01 15:27:05 +0100

    initial code dump

---
+++ mdbench.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import os
+import socket
+from datetime import datetime
+
+DIR = 'dir.'
+FILE = 'file.'
+
+gen_dir = lambda base, gen : '%s/%s%d' % (base, DIR, gen)
+gen_file = lambda base, gen : '%s/%s%d' % (base, FILE, gen)
+
+def make_dirs(root, count):
+	for i in range(count):
+		os.mkdir( gen_dir(root, i) )
+
+def make_files(root, dir_count, file_count):
+	for i in range(dir_count):
+		for j in range(file_count):
+			f = open(gen_file( gen_dir(root, i), j ), 'w')
+			f.close()
+
+def del_files(root, dir_count, file_count):
+	for i in range(dir_count):
+		for j in range(file_count):
+			os.remove(gen_file( gen_dir(root, i), j ))
+
+def del_dirs(root, count):
+	for i in range(count):
+		os.rmdir( gen_dir(root, i) )
+
+def stat_dirs(root, count):
+	for i in range(count):
+		os.stat( gen_dir(root, i) )
+
+def stat_files(root, dir_count, file_count):
+	for i in range(dir_count):
+		for j in range(file_count):
+			os.stat(gen_file( gen_dir(root, i), j ))
+
+
+def bench_run(func, *args):
+	start = datetime.now()
+
+	result = func(*args)
+
+	end = datetime.now()
+	elapsed = end - start
+
+	return elapsed, result
+
+
+if __name__ == '__main__':
+	
+	dir_count = 1000
+	file_count = 10
+	root = 'mdbench.%s.%d' % (socket.gethostname(), os.getpid())
+
+	os.mkdir(root)
+	elapsed, result = bench_run( make_dirs, root, dir_count )
+	in_sec = elapsed.total_seconds()
+	print '%.2f dir creates per second' % (dir_count/in_sec)
+
+	elapsed, result = bench_run( make_files, root, dir_count, file_count )
+	in_sec = elapsed.total_seconds()
+	print '%.2f files creates per second' % ((dir_count*file_count)/in_sec)
+
+	elapsed, result = bench_run( stat_files, root, dir_count, file_count )
+	in_sec = elapsed.total_seconds()
+	print '%.2f files stats per second' % ((dir_count*file_count)/in_sec)
+
+	elapsed, result = bench_run( del_files, root, dir_count, file_count )
+	in_sec = elapsed.total_seconds()
+	print '%.2f files removes per second' % ((dir_count*file_count)/in_sec)
+
+	elapsed, result = bench_run( del_dirs, root, dir_count )
+	in_sec = elapsed.total_seconds()
+	print '%.2f dir removes per second' % (dir_count/in_sec)
