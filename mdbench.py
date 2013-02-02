b1b85008524260bf439aa46ca07ab401fe2f640b
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2013-02-02 17:11:30 +0100

    fix argument order in mkfile

--- mdbench.py
+++ mdbench.py
@@ -34,7 +34,7 @@ def make_dirs(root, count):
 def make_files(root, dir_count, file_count, size = 0):
 	for i in range(dir_count):
 		for j in range(file_count):
-			mkfile(gen_file( gen_dir(root, i), j ), 1024, size)
+			mkfile(gen_file( gen_dir(root, i), j ), size, 1024)
 
 def del_files(root, dir_count, file_count):
 	for i in range(dir_count):
